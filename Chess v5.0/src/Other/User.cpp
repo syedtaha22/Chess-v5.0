93a5cd053def069b31de4dd4a5b2df5aab068c7f
Syed Taha <syetaha@gmail.com>
2024-05-01 02:18:33 +0500

    Removed EloManager manager, Instead Now using Settings

--- Chess v5.0/src/Other/User.cpp
+++ Chess v5.0/src/Other/User.cpp
@@ -2,18 +2,17 @@
 
 User::User(string name, int elo) : Username(name), ELO(elo) {
     //if No ELO is provided, get ELO from settings;
-    if (ELO == 0) ELO = manager.get(manager.userElo);
-    //if ELO is still Zero, meaning no prior setting has been saved, then assigne default
+    if (ELO == 0) ELO = Settings::get(Settings::userElo);
+    //if ELO is still Zero, meaning no prior setting has been saved, then assign default
     if (ELO == 0) ELO = 500; //Default
 }
 
 
 void User::setUserName(string name) {
     Username = name;
-    ELO = manager.get(manager.userElo);
+    ELO = Settings::get(Settings::userElo);
     if (ELO == 0) ELO = 500; //Default
 
 }
-
 
 
