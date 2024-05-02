2bf80485931ec781b12142bb49be9af8e072e37c
Syed Taha <syetaha@gmail.com>
2024-05-02 23:29:39 +0500

    Added Logic from Menu.h to Menu.cpp

--- Chess v5.0/src/Other/Menu.cpp
+++ Chess v5.0/src/Other/Menu.cpp
@@ -1,3 +1,26 @@
 #include "../../headers/Other/Menu.h"
 
+Menu::Menu() {
+    InfoBox = { InfoBoxX, InfoBoxY, InfoBoxWidth, InfoBoxHeight };
+    StartButton = { buttonX, StartButtonY, width, height };
+    MultiplayerButton = { buttonX, MultiPlayerbuttonY, width, height };
+    FENButton = { buttonX, FenButtonY, width, height };
+    SettingsMenu = { buttonX, SettingMenuY , width, height };
 
+    StartSingleplayer = false;
+    StartMultiplayer = false;
+    LoadFromFen = false;
+    OpenSettings = false;
+
+}
+
+void Menu::ShowMenu() {
+    StartSingleplayer = GuiButton(StartButton, "Start");
+    StartMultiplayer = GuiButton(MultiplayerButton, "Multiplayer");
+    LoadFromFen = GuiButton(FENButton, "Load From FEN\n(New)");
+    OpenSettings = GuiButton(SettingsMenu, "Settings\n(New)");
+}
+
+bool Menu::isOptionPressed() const { return StartSingleplayer || StartMultiplayer; }
+
+void Menu::DrawMenuBox() const { DrawRectangleRounded(InfoBox, Roundedness, Segments, Translucent); }
