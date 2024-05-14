581cf4aa91a2e6a44f746f68396319f139eadaf2
Syed Taha <syetaha@gmail.com>
2024-05-15 01:02:57 +0500

    Changes to testing script

--- Chess v5.0/Tests/start_test.sh
+++ Chess v5.0/Tests/start_test.sh
@@ -2,12 +2,12 @@
 
 # Check if the correct number of arguments is provided
 if [ "$#" -lt 1 ] || [ "$#" -gt 3 ]; then
-    echo "Usage: $0 <FEN database file> [loop_limit]"
+    echo "Usage: $0 <FEN database file> [loop_limit] [Depth]"
     exit 1
 fi
 
 # Path to the text file containing FEN strings
-FEN_FILE="$2"
+FEN_FILE="$1"
 
 CPP_PROGRAM="./test_engine.exe"
 
@@ -25,13 +25,19 @@ fi
 
 # Set the loop limit
 if [ "$#" -eq 2 ]; then
-    LOOP_LIMIT=$1
+    LOOP_LIMIT=$2
 else
     LOOP_LIMIT=-1  # Default value, meaning run the loop for the complete file
 fi
 
+# Set the Depth
+if [ "$#" -eq 3 ]; then
+    Depth=$3
+else
+    Depth=2  # Default value
+fi
+
 # Iterate over each FEN string in the file
-Depth=2
 counter=0
 while IFS= read -r fen && ([ "$LOOP_LIMIT" -eq -1 ] || [ "$counter" -lt "$LOOP_LIMIT" ]); do
     # Execute your C++ program with the FEN string as argument
