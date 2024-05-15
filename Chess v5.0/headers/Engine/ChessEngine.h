d74de564d50d14200cb3d4be6f6f397f8d4e91e4
Syed Taha <syetaha@gmail.com>
2024-05-16 02:34:15 +0500

    Applied Abstraction and Removed pst
    -pst is now a global variable defined in Pieces.h and renamed to PST

--- Chess v5.0/headers/Engine/ChessEngine.h
+++ Chess v5.0/headers/Engine/ChessEngine.h
@@ -3,25 +3,26 @@
 
 
 #include "EngineState.h"
-#include "PieceSquareTables.h"
+
 
 class ChessEngine{
-public:
-    PieceSquareTables pst;
-    EngineState state;
-    
-    ChessEngine(int Color = EMPTY, bool loadSettings = true);
-
-    //Move Searching Functions
-    void shuffleMoves(std::vector<std::string>& possibleMoves);
-    std::string GenerateMove(const ChessBoard& board);
 
     int Minimax(ChessBoard& board, int depth, int alpha, int beta, auto time, int currentPlayer);
 
     //Evaluation Functions
     int Evaluate(const ChessBoard& chessboard, int currentPlayerColor) const;
-
     void adjustEndgamePositionalAdvantage(const ChessBoard& chessboard, int currentPlayerColor, int& positionalAdvantage) const;
+
+    void shuffleMoves(std::vector<std::string>& possibleMoves);
+
+public:
+
+    EngineState state;
+    
+    ChessEngine(int Color = EMPTY, bool loadSettings = true);
+
+    //Move Searching Functions
+    std::string GenerateMove(const ChessBoard& board);
 
     void PlayMove(const std::string& move, ChessBoard& board) const;
 
