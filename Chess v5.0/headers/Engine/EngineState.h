21cea8825ad0a7df4523d60c47bc6981c39490c9
Syed Taha <syetaha@gmail.com>
2024-05-16 02:35:59 +0500

    Applied Abstraction and added methods to get and set private members

--- Chess v5.0/headers/Engine/EngineState.h
+++ Chess v5.0/headers/Engine/EngineState.h
@@ -5,38 +5,41 @@
 #include "ChessEngineStatistics.h"
 
 class EngineState {
-    
-
-public:
-    //Represents the Color the Engine Plays as(Black by Default)
-    int EngineColor;
 
     // Maximum depth for the Minimax algorithm
     int MAX_DEPTH;
 
+    //Represents the Color the Engine Plays as(Black by Default)
+    int EngineColor;
+
+    //Time Limit for Search, (0 means no Time limit)
+    float timeLimit = 0;
+
+    const std::unordered_map<int, int> pieceValues = { { PAWN, 100 }, { KNIGHT, 320 }, { BISHOP, 330 }, { ROOK, 500 }, { QUEEN, 900 }, { KING, 1000 } };
+    
+    int engineEloRating;
+
+    int getELOFromSettings() const;
+
+public:
     //Transpostion Tables to Store Positional Data
     TranspositionTables transpostionTable;
+    //Flags Defining Wether to Use Features
+    const bool useTranspositions = true;
+    const bool useAlphaBetaPruning = true;
 
     //Flags to start or End search
     bool terminateSearch;
     bool startSearch;
 
-    //Time Limit for Search, (0 means no Time limit)
-    const float timeLimit = 0;
-
-    //Flags Defining Wether to Use Features
-    const bool useTranspositions = true;
-    const bool useAlphaBetaPruning = true;
-
-    const std::unordered_map<int, int> pieceValues = { { PAWN, 100 }, { KNIGHT, 320 }, { BISHOP, 330 }, { ROOK, 500 }, { QUEEN, 900 }, { KING, infinity } };
-
-    int engineEloRating;
-
     EngineState();
 
+    int GetEngineColor() const;
     void setEngineColor(int color);
-    void LoadSavedSettings();
 
+    float getTimeLimit() const;
+
+    void LoadSavedSettings();
 
 
     void TerminateSearch();
@@ -45,13 +48,11 @@ public:
     // Set the stop flag to stop the search
     void StopSearching();
 
+    int getELO() const;
+    void setElO(int newELO);
 
-
-    int getELO();
     int getDepth() const;
-
     void setDepth(int NewDepth);
-
 
     void SaveTranspositionTable();
     double getSizeOfTranspositionTable() const;
