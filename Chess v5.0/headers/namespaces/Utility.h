777501108015c55c3369285816212b42c992e3c5
Syed Taha <syetaha@gmail.com>
2024-05-24 19:19:10 +0500

    Added Functions to Load, Unload Sound and Fonts. Also Refactored Engine Thread function to Engine_Calculations

--- Chess v5.0/headers/namespaces/Utility.h
+++ Chess v5.0/headers/namespaces/Utility.h
@@ -16,6 +16,15 @@ namespace Utility {
 	//returns the cordinates of center of a text. that can my used to position the text properly
 	Vector2 TextCenter(const char* text, float fontSize);
 
+	void UnloadFontsAndSounds();
+
+	void LoadFontsAndSounds();
+
+	//Supress Raylib Log messages
+	void SuppressRaylibLog(int msgType, const char* text, va_list args);
+
+	void Engine_Calculations(ChessEngine& engine, ChessBoard& board);
+
 }
 
 #endif // !HELPER_FUNCTIONS_H
