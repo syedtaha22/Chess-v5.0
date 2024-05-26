f7f664c16a9c6018044960653fda465668993970
Syed Taha <syetaha@gmail.com>
2024-05-26 22:47:26 +0500

    Added Color variables

--- Chess v5.0/headers/Board/GraphicalBoard.h
+++ Chess v5.0/headers/Board/GraphicalBoard.h
@@ -4,6 +4,16 @@
 #include "ChessBoard.h"
 
 class GraphicalBoard {
+    const Color lightSquare = { 191, 208, 204, 255 }; //Light Green
+    const Color darkSquare = { 66, 138, 152, 255 }; //Teal
+
+    //const Color lightSquare = { 238, 238, 210, 255 }; //Light Yellow
+    //const Color darkSquare = { 118, 150, 86, 255 }; // Green
+
+    const Color MoveHighlight = Fade({ 255, 255, 0, 200 }, 0.5);
+    const Color MovesForPieceHighLight = Fade({ 255, 0, 0, 200 }, 0.5);
+    const Color MoveHighlightBlue = Fade({ 0, 0, 255, 200 }, 0.5);
+    const Color NextMoveHighlight = Fade({ 0, 255, 0, 200 }, 0.5);
 
     const float TextSize = 15;
 
