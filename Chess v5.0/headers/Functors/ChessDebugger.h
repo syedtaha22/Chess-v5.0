40f3b05a1890470e45c89fed3256eede69e63581
Syed Taha <syetaha@gmail.com>
2024-05-26 22:27:53 +0500

    Made certain logical improvements

--- Chess v5.0/headers/Functors/ChessDebugger.h
+++ Chess v5.0/headers/Functors/ChessDebugger.h
@@ -6,28 +6,30 @@
 
 class ChessDebugger {
     // Base case function to print the last argument
-    void print() {
-        std::cout << std::endl;
+    void print_args(std::ostream& os) {
+        os << std::endl;
     }
 
-
+    // Recursive variadic template function to print arguments
+    template<typename T, typename... Args>
+    void print_args(std::ostream& os, const T& firstArg, const Args&... args) {
+        os << firstArg << " ";
+        print_args(os, args...); // Recur for the remaining arguments
+    }
 
 public:
-    // Display Vector on Terminal
-    void moves(const std::vector<std::string>& moves) {
-        for (const auto& move : moves) {
-            std::cout << move << " ";
-        }
+
+    //Display Vector on Terminal
+    static void moves(const std::vector<std::string>& moves_) {
+        for (const auto& move : moves_) { std::cout << move << " "; }
         std::cout << std::endl;
     }
 
  
     // Wrapper function to allow usage with std::cout
-    // Recursive variadic template function to print arguments
-    template<typename T, typename... Args>
-    void print(const T& firstArg, const Args&... args) {
-        std::cout << firstArg << " ";
-        print(args...); // Recur for the remaining arguments
+    template<typename... Args>
+    void operator()(Args&&... args) {
+        print_args(std::cout, std::forward<Args>(args)...);
     }
 
 };
