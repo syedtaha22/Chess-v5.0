257c32fc3543b66a0f6cf5c9224ea1a753e5b9a6
Syed Taha <syetaha@gmail.com>
2024-05-26 23:00:43 +0500

    Removed initialisation of color variables as they have been moved to classes that need them

--- Chess v5.0/src/Other/GlobalVariables.cpp
+++ Chess v5.0/src/Other/GlobalVariables.cpp
@@ -8,14 +8,4 @@ Sound GameEnds;
 
 Font myFont;
 
-Color lightSquare = { 238, 238, 210, 255 };
-Color darkSquare = { 118, 150, 86, 255 };
-Color Transparent = { 0, 0, 0, 0 };
 Color Background = { 49, 46, 43, 255 };
-Color Translucent = { 0, 0, 0, 64 };
-
-Color MoveHighlight = Fade({ 255, 255, 0, 200 }, 0.5);
-Color MovesForPieceHighLight = Fade({ 255, 0, 0, 200 }, 0.5);
-Color MoveHighlightBlue = Fade({ 0, 0, 255, 200 }, 0.5);
-Color NextMoveHighlight = Fade({ 0, 255, 0, 200 }, 0.5);
-
